1857a7993738af1831847238f5e299a76d46c363
Omar H <omarhayatusa@gmail.com>
2025-03-14 08:44:45 -0500

    Update

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 document.querySelector('form').addEventListener('submit', event => 
-    {event.preventDefault()});
+    {event.preventDefault();
 
-const input = document.querySelector('input');
+        document.querySelector('#message-form').classList.add('hide');
+        document.querySelector('#link-form').classList.remove('hide');
 
-console.log(input.value);
+const input = document.querySelector('#message-input');
+const encrypted = btoa(input.value);
+
+const linkInput = document.querySelector('#link-input')
+linkInput.value = `${window.location}#${encrypted}`;
+linkInput.select();
+
+});
